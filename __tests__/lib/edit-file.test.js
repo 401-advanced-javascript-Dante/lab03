c9d0365d46304a17b22b02b48a48e458e4c73d61
dantemessy <thepunishar1@gmail.com>
2020-01-21 22:36:05 +0200

    Accepts a file name as a command line parameter

--- __tests__/lib/edit-file.test.js
+++ __tests__/lib/edit-file.test.js
@@ -7,21 +7,56 @@
 
 
 const editor = require('../../lib/edit-file') ;
-
-
+const path = require("path");
 describe('file modules' , () => {
 
+    describe('using promise ' , () => {
+        it('check if i can read the file as a string' , () => {
+            let file = `${__dirname}/../../data/person.json`;
+            // let fileName = path.basename(file);
+            // process.argv.push(fileName);
+            // console.log(fileName) ;
+            return editor.readerFunctionPromise(file)
+                .then( (data) => { 
+                    expect(typeof(data.toString().trim())).toEqual('string')
+                })
+        })
 
-describe('callback method' , () => {
-    it('check the error if the name is wrong' , (check) => {
-        let file = `${__dirname}/../../data/person.json`;
-        editor.readerFunctionCallback(file , (err ,data) => {
-            console.log(data.toString());
-            expect(err).toBeDefined();
-            check();
-        });
-    });
-});
+        it('check if Accepts a file name as a command line parameter' , () => {
+            let file = `${__dirname}/../../data/person.json`;
+            let fileName = path.basename(file);
+            process.argv.push(fileName);
+            expect(process.argv[process.argv.length-1]).toEqual(fileName);
+        })
+
+        it('Alter some values in the object' , () => {
+            let file = `${__dirname}/../../data/person.json`;
+            let data = 'ali' ;
+            return editor.writerFunctionPromise(file , data )
+                .then( (output) => {
+                    expect(output.firstName).toEqual('ali');
+                } )
+
+        })
+
+
+    
+    })  
+
+
+
+// describe('callback method' , () => {
+//     it('check the error if the name is wrong' , (check) => {
+//         let file = `${__dirname}/../../data/person.json`;
+//         editor.readerFunctionCallback(file , (err ,data) => {
+//             console.log('check the data', data.toString());
+//             console.log(process.argv);
+
+//             expect(err).toBeDefined();
+//             check();
+//         });
+//     });
+// });
 
 
 
